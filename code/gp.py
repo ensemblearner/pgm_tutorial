3cb52b8bbc2ba25ec5b37c40cdf6ea37e6457645
maverick <mohit1007@gmail.com>
2016-08-27 00:03:29 -0700

    fix nits

--- code/gp.py
+++ code/gp.py
@@ -1,7 +1,9 @@
 import numpy as np
+
+
 class GPReg(object):
 
-    def __init(self, X, y, kfunc):
+    def __init__(self, X, y, kfunc):
         self.X = X
         self.y = y
         self.m, self.n = X.shape
@@ -16,20 +18,15 @@ class GPReg(object):
                     kernel[j, k] = self.kfunc(ele, ele_2)
         return kernel
 
-
     def mean_post_infer(self, K_star):
         return K_star.dot(np.linalg.inv(self.K)).dot(self.y)
 
     def covar_post_infer(self, K_star, K_star_star):
         return K_star_star - K_star.dot(np.linalg.inv(self.K)).K_star.T
 
-
-    def predict(self, x_star, y_star):
+    def predict(self, x_star):
         K_star_star = self.kfunc(x_star, x_star)
         K_star = np.matrix([self.kfunc(x, x_star) for x in self.X])
         mu_post = self.mean_post_infer(K_star)
         covar_post = self.covar_post_infer(K_star, K_star_star)
         return mu_post, covar_post
-    
-
-
